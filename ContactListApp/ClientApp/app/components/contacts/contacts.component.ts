e8de97765ccb78193316b14a664a24c94c19dfb2
Tomislav Maslač <tm48782@fer.hr>
2017-09-11 11:50:18 +0200

    Omogućeno brisanje kontakata; početak rada na modalu za dodavanje novih kontakata

--- ContactListApp/ClientApp/app/components/contacts/contacts.component.ts
+++ ContactListApp/ClientApp/app/components/contacts/contacts.component.ts
@@ -6,21 +6,52 @@ import { Http } from '@angular/http';
     templateUrl: './contacts.component.html'
 })
 export class ContactsComponent {
+	private http : Http;
     public contacts: Contact[];
-	public selectedContact : Contact;
+	public tempMessage : TempMessage;
+	public newContact : Contact = {
+		id : 0,
+		firstName : "",
+		lastName : "",
+		address : ""
+	};
 
     constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
         http.get(baseUrl + 'api/contacts').subscribe(result => {
             this.contacts = result.json() as Contact[];
         }, error => console.error(error));
+		this.http = http;
     }
 
-	onSelect(contact : Contact): void {
-		this.selectedContact = contact;
+	deleteContact(contact : Contact) : void {
+		if(confirm("Jeste li sigurni?")){
+			this.http.delete("api/Contacts/" + contact.id)
+				.subscribe(response => {
+					this.contacts.splice(this.contacts.indexOf(contact), 1);
+					this.showTempMessage("Kontakt " + contact.firstName + " " + contact.lastName + " uspješno obrisan!", true);
+				},
+				error => {
+					this.showTempMessage("Greška pri brisanju kontakta!", false);
+				});
+		}
+	}
+
+	showTempMessage(message : string, status : boolean) : void {
+		this.tempMessage = {
+			message : message,
+			status : status
+		};
 	}
 }
 
 interface Contact {
 	id: number;
-    fullName: string;
+    firstName: string;
+	lastName: string;
+	address : string;
+}
+
+interface TempMessage {
+	message : string;
+	status : boolean;
 }
